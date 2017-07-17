0270ecba052f1bb29f43ad0996abdd322f3baf91
martec <mr.martchero@gmail.com>
2017-07-17 13:32:16 +0900

    try fix travis3

---
+++ travis/prepare-epv.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# This file is part of the phpBB Forum Software package.
+#
+# @copyright (c) phpBB Limited <https://www.phpbb.com>
+# @license GNU General Public License, version 2 (GPL-2.0)
+#
+# For full copyright and license information, please see
+# the docs/CREDITS.txt file.
+#
+set -e
+set -x
+
+NOTESTS=$1
+
+if [ "$NOTESTS" == "1" ]
+then
+	cd phpBB
+	composer require phpbb/epv:dev-master --dev --no-interaction
+	cd ../
+fi
